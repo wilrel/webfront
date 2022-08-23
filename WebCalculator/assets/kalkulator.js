3a569b6bf34ebfb03444ae57939ee0b6df99a239
wilrel <wildannasrulloh321@gmail.com>
2022-08-23 18:56:42 +0700

    Menambah Function

--- WebCalculator/assets/kalkulator.js
+++ WebCalculator/assets/kalkulator.js
@@ -1,1 +1,41 @@
-console.log("Selamat Anda berhasil menggunakan JavaScript pada Website");
+const calculator = {
+  displayNumber: '0',
+  operator: null,
+  firstNumber: null,
+  isWaitForSecondNumber: false,
+};
+
+function updateDisplay() {
+  document.querySelector('#displayNumber').innerText = calculator.displayNumber;
+}
+
+function clearCalculator() {
+  calculator.displayNumber = '0';
+  calculator.operator = null;
+  calculator.firstNumber = null;
+  calculator.isWaitForSecondNumber = false;
+}
+
+function inputDigit(digit) {
+  if (calculator.displayNumber === '0') {
+    calculator.displayNumber = digit;
+  } else {
+    calculator.displayNumber += digit;
+  }
+}
+
+const buttons = document.querySelectorAll('.button');
+
+for (const button of buttons) {
+  button.addEventListener('click', function (event) {
+    //mendapatkan objek elemen yang diklik
+    const target = event.target;
+
+    if (target.classList.contains('clear')) {
+
+    }
+    inputDigit(target.innerText);
+    updateDisplay();
+
+  });
+}
